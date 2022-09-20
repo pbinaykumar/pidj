988adaa408254c6ae61109c8a643e48aedfe70cd
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2022-09-20 22:36:15 +0530

    Update pigpio.py

--- drive/pigpio.py
+++ drive/pigpio.py
@@ -1,16 +1,23 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-lmin1 = 24
-lmin2 = 23
-lmen = 25
-
+lmin1 = 14
+lmin2 = 15
+lmen = 18
+rmin1 = 23
+rmin2 = 24
+rmen = 25
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(lmin1, GPIO.OUT)
 GPIO.setup(lmin2, GPIO.OUT)
 GPIO.setup(lmen, GPIO.OUT)
+GPIO.setup(rmin1, GPIO.OUT)
+GPIO.setup(rmin2, GPIO.OUT)
+GPIO.setup(rmen, GPIO.OUT)
 GPIO.output(lmin1, GPIO.LOW)
 GPIO.output(lmin2, GPIO.LOW)
+GPIO.output(rmin1, GPIO.LOW)
+GPIO.output(rmin2, GPIO.LOW)
 p = GPIO.PWM(lmen, 2000)
 p.start(90)
 
@@ -19,12 +26,16 @@ def forwordMotion():
     print('bbbvbvvv')
     GPIO.output(lmin1, GPIO.HIGH)
     GPIO.output(lmin2, GPIO.LOW)
+    GPIO.output(rmin1, GPIO.HIGH)
+    GPIO.output(rmin2, GPIO.LOW)
     return 0
 
 
 def backwordMotion():
     GPIO.output(lmin1, GPIO.LOW)
     GPIO.output(lmin2, GPIO.HIGH)
+    GPIO.output(rmin1, GPIO.LOW)
+    GPIO.output(rmin2, GPIO.HIGH)
     return 0
 
 
